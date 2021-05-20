13835fa6e3c636ed902d81eec001066b99a1df3a
pixel-pixel <andriyzbartish@gmail.com>
2021-05-20 17:49:37 +0300

    now lab is done(realy...)

--- lab7/OrderOfElement.h
+++ lab7/OrderOfElement.h
@@ -5,16 +5,15 @@
 #include <vector>
 using namespace std;
 
-#include "../Number.h"
-#include "../Factorization.h"
-
+#include "../Lab1GA/Number.h"
+#include "../Lab1GA/Factorization.h"
 
 namespace LongArithmetic {
-        Number calcPower(const Number &bottom, const Number &top,  const Number &modulo);
+    //Number calcPower(const Number &bottom, const Number &top,  const Number &modulo);
 
+    Number gcd(const Number &a, const Number &b);
+    //n - Модуль, a - Елемент
     Number find(const Number &n, const Number &a);
-
-
 }
 
 
